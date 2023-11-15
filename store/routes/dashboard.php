c8a1115f7cd98e03a82b9ef05cfd55229b40ef50
Basmalakh12 <basmala.kh@icloud.com>
2023-11-15 21:41:56 +0200

    store project

---
+++ store/routes/dashboard.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Http\Controllers\Dashboard\CtegoriesController;
+use App\Http\Controllers\DashboardController;
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/dashboard',[DashboardController::class, 'index'])
+     ->middleware(['auth'])
+     ->name('dashboard');
+
+Route::resource("dashboard/categories",CtegoriesController::class);
+
+
+
+
+
+
+
+
+
+
+
+?>
